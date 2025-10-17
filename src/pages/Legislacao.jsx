05e782c0fadff18c3f6e6716014d23ce58d59e29
Guilherme Da Silva Vieira <guilherme_silva_vieira@proton.me>
2025-10-17 17:10:41 -0300

    inicio página legislação

--- src/pages/Legislacao.jsx
+++ src/pages/Legislacao.jsx
@@ -3,12 +3,17 @@ import NavBar from '../components/Navbar'
 import Footer from '../components/Footer'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-function Legislacao() {
+const Legislacao = () => {
   return (
 	<>
     		<NavBar/>
 		<main>
 			<h1 className="text-center">Lei Carolina Dieckmann</h1>
+			<div className="m-4">	
+				<p className="fs-3 text-center"><img src="img/legislacao.jpg" alt="balança e martelo" className="h-auto w-25"/></p>
+				<p className="fs-3 m-2 text-justify">Definiu os diferentes tipos de crimes virtuais, entrando em vigor no dia 3 de abril de 2013.</p>
+				
+			</div>
 		</main>
     		<Footer/>
 	</>
