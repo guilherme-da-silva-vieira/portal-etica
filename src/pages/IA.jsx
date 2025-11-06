995584fa204fdc6094911550a696091edc3c15ff
alunolages <alunolages@lages.sc>
2025-11-06 15:45:29 -0300

    adicionando a página inteligência artificial e Sobre

--- src/pages/IA.jsx
+++ src/pages/IA.jsx
@@ -2,10 +2,31 @@ import React from 'react'
 import NavBar from '../components/Navbar'
 import Footer from '../components/Footer'
 
-function IA() {
+const artigo = () => {
+	const conteudos = [
+	{"id":0,"mensagem":"O vídeo abaixo apresenta o Guilherme da Silva Vieira fazendo malabarismo, mesmo não sendo ele, este tipo de vídeo é chamado de deepfake;"},
+	{"id":1,"mensagem":"Deepfake é a síntese de imagens ou até mesmo sons humanos gerados por inteligência artificial;"},
+	{"id":2,"mensagem":"Sendo algo bastante preocupante pois pode ser usado por exemplo para a disseminação de notícias falsas durante os períodos eleitorais;"},
+	{"id":3,"mensagem":`Além disso, também pode ser utilizados para gerar vídeos íntimos falsos, por exemplo na Coreia do Sul, os vídeos falsos estavam sendo enviados para a plataforma de mensagens Telegram, atualmente
+	a Polícia Nacional da Coreia do Sul está investigando sobre o assunto;`},
+	{"id":4,"mensagem":`Outro Exemplo seriam os chatbots que se autointitulam "Deus" e "Jesus", o principais riscos destes chatbots é que pode ser utilizados para manipular questões de fé, política e financeira.`}];
+	return conteudos;
+}
+
+const IA = () => {
+  const topicos = artigo(); 
   return (
     <>
         <NavBar/>
+	<h1 className="text-center text-decoration-underline">Inteligência Artificial</h1>
+	<div className="text-center">
+		<video controls width="250">
+			<source src="/videos/malabarismo.mp4" type="video/mp4" />
+		</video>
+		<ul className="list-group w-25 m-auto">
+			{topicos.map(topico => <li key={topico.id} className="list-group-item text-justify">{topico.mensagem}</li>)}
+		</ul>
+	</div>
         <Footer/>
     </>
   )
--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -6,6 +6,40 @@ function Sobre() {
   return (
     <>
         <NavBar/>
+		<h1 className="text-center text-decoration-underline">Sobre o WebSite</h1>
+		<div class="accordion w-50 m-auto" id="sobre">
+  		<div class="accordion-item">
+    			<h2 class="accordion-header">
+      				<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#alunoPesquisador" aria-expanded="false" aria-controls="alunoPesquisador">
+        				Aluno pesquisador
+      				</button>
+    			</h2>
+    			<div id="alunoPesquisador" class="accordion-collapse collapse" data-bs-parent="#sobre">
+      				<div class="accordion-body"><strong>Guilherme da Silva Vieira</strong></div>
+   		 	</div>
+  		</div>
+  		<div class="accordion-item">
+    			<h2 class="accordion-header">
+      				<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#noticias" aria-expanded="false" aria-controls="noticias">
+       		 			Noticias
+      				</button>
+   			 </h2>
+    			<div id="noticias" class="accordion-collapse collapse" data-bs-parent="#sobre">
+      				<div class="accordion-body">Placeholder content for this accordion, which is intended to demonstrate the <code>.accordion-flush</code> class. This is the second item’s accordion body. Let’s imagine this being filled with some actual content.</div>
+    		</div>
+  	</div>
+	<div class="accordion-item">
+    		<h2 class="accordion-header">
+      			<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#imagens" aria-expanded="false" aria-controls="imagens">
+        			Imagens
+      			</button>
+    		</h2>
+    		<div id="imagens" class="accordion-collapse collapse" data-bs-parent="sobre">
+      				<div class="accordion-body"><strong>Guilherme da Silva Vieira</strong></div>
+   		 </div>
+  	</div>
+</div>
+
 	<Footer/>
     </>
   )
