80b189a7cccae42576d469d8b1356a69c21f492c
guilherme_silva_vieira@proton.me <guilherme_silva_vieira@proton.me>
2025-11-13 14:58:20 -0300

    Correção de digitação

--- src/pages/IA.jsx
+++ src/pages/IA.jsx
@@ -4,7 +4,7 @@ import Footer from '../components/Footer'
 
 const artigo = () => {
 	const conteudos = [
-	{"id":0,"mensagem":"O vídeo abaixo apresenta o Guilherme da Silva Vieira fazendo malabarismo, mesmo não sendo ele, este tipo de vídeo é chamado de deepfake;"},
+	{"id":0,"mensagem":"O vídeo acima apresenta o Guilherme da Silva Vieira fazendo malabarismo, mesmo não sendo ele, este tipo de vídeo é chamado de deepfake;"},
 	{"id":1,"mensagem":"Deepfake é a síntese de imagens ou até mesmo sons humanos gerados por inteligência artificial;"},
 	{"id":2,"mensagem":"Sendo algo bastante preocupante pois pode ser usado por exemplo para a disseminação de notícias falsas durante os períodos eleitorais;"},
 	{"id":3,"mensagem":`Além disso, também pode ser utilizados para gerar vídeos íntimos falsos, por exemplo na Coreia do Sul, os vídeos falsos estavam sendo enviados para a plataforma de mensagens Telegram, atualmente
